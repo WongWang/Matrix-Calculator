fef7d0db487f121e9d0cddace86d5a30ac22f8b1
Castell_Windows <border_05masons@icloud.com>
2022-12-26 14:50:06 +0800

    Finished the +=, -= and *= operation

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,10 +1,5 @@
 #include "Matrix.h"
 
-inline bool Matrix::is_same_size(const Matrix &rightSide) const
-{
-    return (rowCnt == rightSide.rowCnt && colCnt == rightSide.colCnt);
-}
-
 Matrix &Matrix::operator=(std::initializer_list<std::initializer_list<int>> &list)
 {
     if (list.begin() == list.end())
@@ -56,12 +51,24 @@ Matrix &Matrix::operator*=(const Matrix &rightSide)//To be finished
     if (colCnt != rightSide.rowCnt)
         throw std::invalid_argument("Column number of the former matrix is not equal to row number of the latter matrix");
     Matrix ans;
-    for (decltype(rowCnt) rowSubscript = 0; rowSubscript != rowCnt; ++rowSubscript)
+    for (decltype(rowCnt) leftRow = 0; leftRow != rowCnt; ++leftRow)
     {
-        for (decltype(colCnt) colSubscript = 0; colSubscript != colCnt; ++colSubscript)
+        std::vector<int> ansLine;
+        for (decltype(colCnt) rightCol = 0; rightCol != rightSide.colCnt; ++rightCol)
         {
-            matrix[rowSubscript][colSubscript] -= rightSide.matrix[rowSubscript][colSubscript];
+            ansLine.emplace_back(dot_product(get_row(leftRow + 1), rightSide.get_column(rightCol + 1)));
         }
+        ans.matrix.emplace_back(ansLine);
     }
+    *this = ans;
     return *this;
+}
+
+std::vector<int> &Matrix::operator[](std::vector<std::vector<int>>::size_type n)
+{
+    return matrix[n]
+}
+const std::vector<int> &Matrix::operator[](std::vector<std::vector<int>>::size_type n) const
+{
+
 }
--- Matrix.h
+++ Matrix.h
@@ -14,17 +14,55 @@ public:
     Matrix &operator+=(const Matrix &rightSide);
     Matrix &operator-=(const Matrix &rightSide);
     Matrix &operator*=(const Matrix &rightSide);
-    int &operator[](std::size_t n);
-    const int &operator[](std::size_t n) const;
+    std::vector<int> &operator[](std::vector<std::vector<int>>::size_type n);
+    const std::vector<int> &operator[](std::vector<std::vector<int>>::size_type n) const;
     //Other operator-like functions
+    std::vector<int> get_row(std::vector<int>::size_type rowNumber) const;
+    std::vector<int> get_column(std::vector<int>::size_type columnNumber) const;
+    static int dot_product(const std::vector<int> &leftSide, const std::vector<int> &rightSide);
 private:
     //Data member
     std::vector<std::vector<int>> matrix;//Store the matrix
-    std::vector<int>::size_type rowCnt;//Matrix row count
-    std::vector<int>::size_type colCnt;//Matrix column count
+    std::vector<int>::size_type rowCnt;//Matrix row count, when the matrix is m by n, this will be m
+    std::vector<int>::size_type colCnt;//Matrix column count, when the matrix is m by n, this will be n
     //Private member functions
-    bool is_same_size(const Matrix &rightSide) const;//Return true if two matrix is of the same size, otherwise return false
+    bool is_same_size(const Matrix &rightSide) const;//Return true if two matrices are of the same size, otherwise return false
 };
+
+//Inline member functions' defination
+/*Private functions*/
+inline bool Matrix::is_same_size(const Matrix &rightSide) const
+{
+    return (rowCnt == rightSide.rowCnt && colCnt == rightSide.colCnt);
+}
+
+/*Public functions*/
+inline std::vector<int> Matrix::get_row(std::vector<int>::size_type rowNumber) const
+{
+    if (rowNumber > rowCnt)
+        throw std::invalid_argument("Row number exceeded the matrix's row number");
+    return matrix[rowNumber - 1];
+}
+
+inline std::vector<int> Matrix::get_column(std::vector<int>::size_type columnNumber) const
+{
+    if (columnNumber > colCnt)
+        throw std::invalid_argument("Column number exceeded the matrix's column number");
+    std::vector<int> ret_val;
+    for (std::vector<int>::size_type i = 1; i != rowCnt + 1; ++i)
+        ret_val.emplace_back(matrix[i - 1][columnNumber - 1]);
+    return ret_val;
+}
+
+inline int Matrix::dot_product(const std::vector<int> &leftSide, const std::vector<int> &rightSide)//Dimensions of the two vectors must be equal! It should be checked before the function is called
+{
+    int ret_val;
+    for (std::vector<int>::size_type i; i != leftSide.size(); ++i)
+    {
+        ret_val += leftSide[i] * rightSide[i];
+    }
+    return ret_val;
+}
 
 //Operator overloading
 Matrix &operator<<(const Matrix &leftSide, const Matrix &rightSide);
